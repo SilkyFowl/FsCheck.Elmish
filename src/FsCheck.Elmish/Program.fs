e6e83ba19ddb27322f90dd028c5ca6d355b51633
SilkyFowl <16532218+SilkyFowl@users.noreply.github.com>
2022-07-16 11:21:24 +0900

    Add comment.

--- src/FsCheck.Elmish/Program.fs
+++ src/FsCheck.Elmish/Program.fs
@@ -25,10 +25,11 @@ module Cmd =
     /// Capture `'Msg` returned by `'Cmd` async.
     let captureMsgAsync (cmd: Cmd<'Msg>) = async { return captureMsg cmd }
 
-    let private printerrormsg expect actual = sprintf "\n\nexpected cmdMsg = %A\n\nactual cmdMsg = %A\n" expect actual
+    let private printerrormsg expect actual =
+        sprintf "\n\nexpected cmdMsg = %A\n\nactual cmdMsg = %A\n" expect actual
 
     /// Generates a asynchronous property-based test that compare `'Msg`.
-    let msgPropertyAsync (expect:Cmd<'Msg>) (actual:Cmd<'Msg>) =
+    let msgPropertyAsync (expect: Cmd<'Msg>) (actual: Cmd<'Msg>) =
         async {
             let! expectMsg = captureMsgAsync expect
             let! actualMsg = captureMsgAsync actual
@@ -40,14 +41,13 @@ module Cmd =
         }
 
     /// Generates a property-based test that compare `'Msg`.
-    let msgProperty (expect:Cmd<'Msg>) (actual:Cmd<'Msg>) =
+    let msgProperty (expect: Cmd<'Msg>) (actual: Cmd<'Msg>) =
         let expectMsg = captureMsg expect
         let actualMsg = captureMsg actual
 
         (Seq.length expectMsg = Seq.length actualMsg
          && actualMsg |> Seq.forall2 (=) expectMsg)
         |@ printerrormsg expectMsg actualMsg
-
 
 type ElmishContainerSimple<'Model, 'Msg>(init, update) =
     let lockObj = obj ()
@@ -66,16 +66,21 @@ type ElmishContainer<'Model, 'Msg>(init, update) =
 
     member _.Update(msg: 'Msg) =
         lock lockObj (fun _ -> state <- fst state |> update msg)
-    
+
+    member private this.DisplayText = this.ToString()
+
     override this.ToString() =
         $"%A{this.State} %A{Cmd.captureMsg this.Cmds}"
 
+/// Type Alias `Operation<ElmishContainerSimple<'ParentModel, 'ParentMsg>, 'Model>`
 type ElmishOperationSimple<'ParentModel, 'ParentMsg, 'Model> =
     Operation<ElmishContainerSimple<'ParentModel, 'ParentMsg>, 'Model>
 
+/// Type Alias `Operation<ElmishContainer<'ParentModel, 'ParentMsg>, 'Model>`
 type ElmishOperation<'ParentModel, 'ParentMsg, 'Model> = Operation<ElmishContainer<'ParentModel, 'ParentMsg>, 'Model>
 
 module ElmishStateMachine =
+    /// Generates a function to create an `ElmishStateMachine` without `Command`.
     let createFuncSimple
         (operations: ('ParentModel -> 'Model)
                          -> ('Msg -> 'ParentMsg)
@@ -97,6 +102,7 @@ module ElmishStateMachine =
                 member __.Next model =
                     operations modelMapper toParentMsg totMsg model }
 
+    /// Generate a function to create an `ElmishStateMachine` that uses `Command`.
     let createFunc
         (operations: ('ParentModel -> 'Model)
                          -> ('Msg -> 'ParentMsg)
@@ -120,15 +126,16 @@ module ElmishStateMachine =
                 member __.Next model =
                     operations modelMapper toParentMsg totMsg model }
 
+    /// Functions for `update` of signatures such as `impl -> Msg -> Model -> Model` to perform DI with partial application.
     let withUpdate
-        update
-        (f: Gen<'Parent>
+        updateFunc
+        (createFunc: Gen<'Parent>
                 -> ('ParentModel -> 'ChildModel)
                 -> ('ChildMsg -> 'ParentMsg)
                 -> ('ParentMsg -> 'ChildMsg)
                 -> ('ParentMsg -> 'ParentModel -> 'Parent)
                 -> Machine<'ParentContainer, 'Child>)
         =
-        fun init toState toParentMsg toMsg plainUpdate -> f init toState toParentMsg toMsg (update plainUpdate)
+        fun init toState toParentMsg toMsg update -> createFunc init toState toParentMsg toMsg (updateFunc update)
 
 
